e547d36b17bf30ad9a8c0fa94f225f20591fa993
kassiayoub <kassi.ayoub@gmail.com>
2022-03-13 16:22:15 +0100

    example 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,33 @@
 package main;
 
+import railway.Failure;
+import railway.Result;
+import railway.Success;
+
 public class Main {
 
     public static void main(String[] args) {
+        example1();
 
+    }
+
+    public static void example1(){
+        Result<Integer, String> integer42 = new Success<>(42);
+        Result<Integer, String> notAnInteger = new Failure<>("not ana integer");
+
+        printResult(integer42);
+        printResult(notAnInteger);
+    }
+
+    public static void printResult(Result<Integer, String> result){
+        result.either(
+                success -> {
+                    System.out.println("value= " + success);
+                    return result;
+                },
+                failure -> {
+                    System.out.println("error= "+ failure);
+                    return result;
+                });
     }
 }
